5075b33661ee6bc169a6ea696b0ff27a765a3534
Keys4Ever <cubillagonzalo@outlook.com>
2024-12-08 18:35:27 -0300

    Refactor: DoubleJump, gem y speedItem heredan de PickableItem así evitamos repeticiones de código.

--- DoubleJump.h
+++ DoubleJump.h
@@ -1,25 +1,11 @@
 #ifndef DOUBLEJUMP_H
 #define DOUBLEJUMP_H
-
-#include <SFML/Graphics.hpp>
 #include "PickableItem.h"
-#include "Player.h"
-#include <cmath>
 
-class DoubleJump {
-private:
-    sf::Texture dJumpTexture;
-    sf::Sprite dJumpSprite;
-    bool visible = true;
-    bool hasBeenPicked = false;
+class DoubleJump: public PickableItem{
 public:
-    DoubleJump();
-    // Setters
-    void setVisible(bool v);
-    void setHasBeenPicked(bool v);
-    // Getters
-    bool getVisible();
-    bool getHasBeenPicked();
-    sf::Sprite& getDoubleJumpSprite();
+	DoubleJump() : PickableItem("images\\doubleJump.png") {}
+
 };
+
 #endif  // DOUBLEJUMP_H
